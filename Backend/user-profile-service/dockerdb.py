43fa4cd4f072500aac841728ebce2da9ddd83429
Iustin07 <daniel.bodnariu@info.uaic.ro>
2024-01-29 22:51:59 +0200

    endpoints for user resources

---
+++ Backend/user-profile-service/dockerdb.py
@@ -0,0 +1,128 @@
+import mysql.connector
+from mysql.connector import Error
+from flask import jsonify
+config = {
+        'user': 'root',
+        'password': 'secret',
+        'host':'localhost',
+        'port': '3306',
+        'database': 'users'
+    }
+
+def get_db_connection():
+    try:
+        connection = mysql.connector.connect(**config)
+        return connection
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+        return None
+
+def get_users():
+    try:
+        connection = get_db_connection()
+        if connection:
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM users")
+            users = cursor.fetchall()
+            cursor.close()
+            connection.close()
+            return jsonify(users), 200
+        else:
+            return "Database connection error", 500
+    except Error as e:
+        print("Error fetching users:", e)
+        return "Error fetching users", 500
+
+def get_users_resources(resource):
+    try:
+        connection = get_db_connection()
+        if connection:
+            query=get_resource(resource)
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute(query)
+            result = cursor.fetchall()
+            cursor.close()
+            connection.close()
+            return jsonify(result), 200
+        else:
+            return "Database connection error", 500
+    except Error as e:
+        print("Error fetching users:", e)
+        return "Error fetching users", 500
+
+def get_resource(resourceName:str):
+    resources_dic={'skills':"SELECT * FROM skills",
+               'cities':"SELECT * FROM cities",
+               'companies':"SELECT * FROM companies"
+               }
+    return resources_dic[resourceName]
+
+def get_user(user_id):
+    try:
+        connection = get_db_connection()
+        if connection:
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM users WHERE user_id = %s", (user_id,))
+            user = cursor.fetchone()
+            cursor.close()
+            connection.close()
+            if user:
+                return jsonify(user), 200
+            else:
+                return "User not found", 404
+        else:
+            return "Database connection error", 500
+    except Error as e:
+        print("Error fetching user:", e)
+        return "Error fetching user", 500
+
+def add_user(data):
+    try:
+        connection = get_db_connection()
+        if connection:
+            cursor = connection.cursor()
+            user_id=data.get('user_id')
+            user_name = data.get('user_name')
+            email = data.get('email')
+            locale = data.get('locale')
+            city = data.get('city')
+            company = data.get('company')
+            is_known = True
+
+            # Inserting data into the database
+            cursor.execute("INSERT INTO users_data (user_id,user_name, email, locale, city, company, isKnown) VALUES (%s,%s, %s, %s, %s, %s, %s)",
+                           (user_id,user_name, email, locale, city, company, is_known))
+            connection.commit()
+            cursor.close()
+            connection.close()
+            return jsonify({"message": "User added successfully"}), 201
+        else:
+            return "Database connection error", 500
+    except Error as e:
+        print("Error adding user:", e)
+        return "Error adding user", 500
+
+#Function used for pass or show firstTimecreen for user
+def check_user(data):
+    try:
+        connection = get_db_connection()
+        if connection:
+            data = data
+            user_id=data.get('user_id')
+            cursor = connection.cursor(dictionary=True)
+            cursor.execute("SELECT isKnown FROM users_data WHERE user_id = %s ", (user_id,))
+            user = cursor.fetchone()
+
+            if user:
+                is_known = user['isKnown']
+                cursor.close()
+                connection.close()
+                return jsonify({"isKnown": is_known}), 200
+            else:
+                add_user(data)
+                return jsonify({"isKnown": 0}), 200
+        else:
+            return "Database connection error", 500
+    except Error as e:
+        print("Error processing request:", e)
+        return "Error processing request", 500
---
+++ Backend/user-profile-service/src/init.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE users;
+USE users;
+
+
+CREATE TABLE users_data (
+    user_id integer  NOT NULL ,
+    user_name VARCHAR(50),
+    email varchar(150),
+  locale VARCHAR(50),
+  city  varchar(50),
+  company varchar(50),
+  isKnown boolean default false,
+  PRIMARY KEY (user_id)
+
+);
+
+
+INSERT INTO users_data
+  (user_id, user_name, email, locale,city,company)
+VALUES
+  (1, 'Johnny','johhnydeep23@gmail.com','ro','Iasi','Profi'),
+  (2, 'Johnny2','johhnydeep234@gmail.com','bg','Iasi','Lidl');
+
+CREATE TABLE skills (
+    id integer  NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50),
+  PRIMARY KEY (id)
+);
+INSERT INTO skills
+  (name)
+VALUES
+  ('Java'),
+    ('Photoshop'),
+  ('Web');
+
+CREATE TABLE cities (
+    id integer  NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50),
+  PRIMARY KEY (id)
+);
+INSERT INTO cities
+  (name)
+VALUES
+  ('Iasi'),
+    ('Bucharest'),
+  ('Cluj');
+CREATE TABLE companies(
+    id integer  NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50),
+  PRIMARY KEY (id)
+);
+INSERT INTO companies
+  (name)
+VALUES
+  ('Apple'),
+    ('Microsoft'),
+  ('Profi');
