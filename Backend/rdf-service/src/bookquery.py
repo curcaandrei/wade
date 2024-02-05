bb91440d5f5130973cdb7901d83dc086f7c7749e
curcaandrei <curcaandrei99@gmail.com>
2024-02-05 02:13:55 +0200

    diagrams. add authors -deploy

--- Backend/rdf-service/src/bookquery.py
+++ Backend/rdf-service/src/bookquery.py
@@ -1,14 +1,19 @@
-
 def generate_sparql_query(book_ids):
     values_clause = "VALUES ?book { " + " ".join(f"<http://example.com/book/{id}>" for id in book_ids) + " }"
     query = f"""
     PREFIX ex: <http://example.com/>
-    SELECT ?book (REPLACE(STR(?book), "^.*/book/", "") AS ?bookId) ?title ?averageRating ?imageUrl
+    SELECT ?book (REPLACE(STR(?book), "^.*/book/", "") AS ?bookId) ?title ?averageRating ?imageUrl (GROUP_CONCAT(DISTINCT ?authorName; separator=", ") AS ?authors)
     WHERE {{
       {values_clause}
       ?book ex:title ?title .
       OPTIONAL {{ ?book ex:averageRating ?averageRating . }}
       OPTIONAL {{ ?book ex:imageUrl ?imageUrl . }}
+      OPTIONAL {{ 
+        ?bookAuthor ex:book ?book .
+        ?bookAuthor ex:author ?author .
+        ?author ex:name ?authorName .
+      }}
     }}
+    GROUP BY ?book ?title ?averageRating ?imageUrl
     """
     return query
